02113070adb8d733c55526cd1a2092650f7d3543
ariasDev <57456319+ariasDev@users.noreply.github.com>
2020-10-31 20:46:49 -0500

    conecting to mongo db

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,49 +3,49 @@ const bcrypt = require('../utilities/bcrypt')
 const jwt = require('../utilities/jwt')
 const nodeMailer = require('../utilities/nodeMailer')
 
-exports.getUsers = async (req, res, next) => {
+exports.getUsers = async(req, res, next) => {
     try {
         let users = await usersService.getUsers()
-        if(users.length === 0){
+        if (users.length === 0) {
             throw 'Not Found'
         }
-        res.status(200).json({"data": users})
+        res.status(200).json({ "data": users })
     } catch (error) {
-        res.status(404).json({"reject": error})
+        res.status(404).json({ "reject": error })
     }
 }
 
-exports.getOneUser = async (req, res, next) => {
+exports.getOneUser = async(req, res, next) => {
     try {
         let response = await usersService.getOneUser(req.query.email)
-        res.send({"response": response })
+        res.send({ "response": response })
     } catch (error) {
-        res.send({"error": error })
+        res.send({ "error": error })
     }
 }
 
-exports.saveUser = async (req, res, next) => {
+exports.saveUser = async(req, res, next) => {
     try {
-        if(req.body.email  && req.body.password  && req.body.fullname){
-            if(validateEmail(req.body.email)){
+        if (req.body.email && req.body.password && req.body.fullname) {
+            if (validateEmail(req.body.email)) {
                 let user = await usersService.getOneUser(req.body.email)
-                if(user.length !== 0) {
+                if (user.length !== 0) {
                     res.status(400)
-                    .json({
-                        "response": 'User already in use',
-                        "error": "E2001",
-                        "errorDescription": "Este correo ya se encuentra registrado"
-                    })
-                }else {
+                        .json({
+                            "response": 'User already in use',
+                            "error": "E2001",
+                            "errorDescription": "Este correo ya se encuentra registrado"
+                        })
+                } else {
                     req.body.password = await bcrypt.encryptPassword(req.body.password)
                     let userSaved = await usersService.saveUser(req.body)
-                    if(userSaved.errors){
+                    if (userSaved.errors) {
                         res.status(400).json({
-                            "response":  userSaved._message,
+                            "response": userSaved._message,
                             "error": "E2004",
                             "errorDescription": userSaved.message
                         })
-                    }else{
+                    } else {
                         let token = await jwt.getToken(req.body.email)
                         let userData = {
                             "userName": req.body.fullname,
@@ -54,18 +54,18 @@ exports.saveUser = async (req, res, next) => {
                             "token": token
                         }
                         res.status(201).json({
-                            "response":  "user Saved",
+                            "response": "user Saved",
                             "userData": userData
                         })
                     }
                 }
-            }else {
+            } else {
                 res.status(400)
                     .json({
-                        "response": "invalid email" ,
-                         "error": "E2008",
-                        "errorDescription": "El correo no cumple con la expresion regular" 
-                });
+                        "response": "invalid email",
+                        "error": "E2008",
+                        "errorDescription": "El correo no cumple con la expresion regular"
+                    });
             }
         } else {
             res.status(400).json({
@@ -76,28 +76,28 @@ exports.saveUser = async (req, res, next) => {
     } catch (error) {
         // debo poner un logger
         res.status(500)
-        .json({
-            "error": "E2020", 
-            "errorDescription" : 'Algo salió mal, estamos trabajando en ello'
-        })
+            .json({
+                "error": "E2020",
+                "errorDescription": 'Algo salió mal, estamos trabajando en ello'
+            })
     }
 }
 
-exports.login = async (req, res, next) => {
+exports.login = async(req, res, next) => {
     try {
-        if(req.body.email  && req.body.password){
-            if(validateEmail(req.body.email)){
+        if (req.body.email && req.body.password) {
+            if (validateEmail(req.body.email)) {
                 let user = await usersService.getOneUser(req.body.email)
-                if(user.length === 0) {
+                if (user.length === 0) {
                     res.status(400)
-                    .json({
-                        "response": 'User are not registered',
-                        "error": "E2002",
-                        "errorDescription": "Usuario no registrado" 
-                    })
-                }else {
+                        .json({
+                            "response": 'User are not registered',
+                            "error": "E2002",
+                            "errorDescription": "Usuario no registrado"
+                        })
+                } else {
                     let compare = await bcrypt.compare(req.body.password, user[0].password)
-                    if(compare){
+                    if (compare) {
                         let token = await jwt.getToken(req.body.email)
                         let userData = {
                             "userName": user[0].fullname,
@@ -106,177 +106,168 @@ exports.login = async (req, res, next) => {
                             "token": token
                         }
                         res.status(200)
-                        .json({
-                            "response": "Welcome" ,
-                            "userData": userData
-                        });
-                    }else{
+                            .json({
+                                "response": "Welcome",
+                                "userData": userData
+                            });
+                    } else {
                         res.status(400)
-                        .json({
-                            "response": "incorrect password" ,
-                            "error": "E2003",
-                            "errorDescription": "Contraseña incorrecta" 
-                        });
-                    }    
+                            .json({
+                                "response": "incorrect password",
+                                "error": "E2003",
+                                "errorDescription": "Contraseña incorrecta"
+                            });
+                    }
                 }
-            }else{
+            } else {
                 res.status(400)
                     .json({
-                        "response": "invalid email" ,
-                         "error": "E2008",
-                        "errorDescription": "El correo no cumple con la expresion regular" 
-                });
+                        "response": "invalid email",
+                        "error": "E2008",
+                        "errorDescription": "El correo no cumple con la expresion regular"
+                    });
             }
-        }else{
+        } else {
             res.status(400).json({
                 "error": "E2005",
                 "errorDescription": "Falta tu correo y/o contraseña"
             })
         }
-    } catch (error){
+    } catch (error) {
         res.status(500)
-        .json({
-                "error": "E2020", 
-                "errorDescription" : "Error inesperado, estamos trabajando en ello"
-        })
+            .json({
+                "error": "E2020",
+                "errorDescription": "Error inesperado, estamos trabajando en ello"
+            })
     }
 }
 
 exports.checkEmail = async(req, res, next) => {
     try {
-        if(req.body.email){
-            if(validateEmail(req.body.email)){
+        if (req.body.email) {
+            if (validateEmail(req.body.email)) {
                 let user = await usersService.getOneUser(req.body.email)
-                if(user.length === 0){
+                if (user.length === 0) {
                     res.status(400)
-                    .json({
-                            "error": "E2007", 
-                            "errorDescription" : "Usuario no registrado"
-                    })
-                }else{
-                    let responseNodeMailer = await nodeMailer.sendVerificationCode(req.body.email, user[0].fullname)
-                    if(responseNodeMailer.error){
-                        throw 'error inesperado'
-                    }else {
-                        await usersService.saveVerificationCode(
-                            {"email": req.body.email},
-                            {"verificationCode": responseNodeMailer.verificationCode}
-                        )
-                        res.status(200)
                         .json({
-                            "response": "Se envió código de verificación con exito", 
+                            "error": "E2007",
+                            "errorDescription": "Usuario no registrado"
                         })
+                } else {
+                    let responseNodeMailer = await nodeMailer.sendVerificationCode(req.body.email, user[0].fullname)
+                    if (responseNodeMailer.error) {
+                        throw responseNodeMailer.error
+                    } else {
+                        await usersService.saveVerificationCode({ "email": req.body.email }, { "verificationCode": responseNodeMailer.verificationCode })
+                        res.status(200)
+                            .json({
+                                "response": "Se envió código de verificación con exito",
+                            })
                     }
                 }
-            }else {
+            } else {
                 res.status(400)
                     .json({
-                        "response": "invalid email" ,
-                         "error": "E2008",
-                        "errorDescription": "El correo no cumple con la expresion regular" 
-                });
+                        "response": "invalid email",
+                        "error": "E2008",
+                        "errorDescription": "El correo no cumple con la expresion regular"
+                    });
             }
-        }else{
+        } else {
             res.status(400)
-            .json({
+                .json({
                     "error": "E2006",
-                    "errorDescription" : "Falta el campo Email"
-            })
+                    "errorDescription": "Falta el campo Email"
+                })
         }
     } catch (error) {
         res.status(500)
-        .json({
-                "error": "E2020", 
-                "errorDescription" : "Error inesperado, estamos trabajando en ello"
-        })
+            .json({
+                "error": "E2020",
+                "errorDescription": "Error inesperado, estamos trabajando en ello"
+            })
     }
 }
 
-exports.checkValidationCode = async (req, res, next) => {
+exports.checkValidationCode = async(req, res, next) => {
     try {
-        if(req.body.verificationCode && req.body.email){
+        if (req.body.verificationCode && req.body.email) {
             let user = await usersService.getOneUser(req.body.email)
-            if(user.length !== 0){
-                if(user[0].verificationCode === req.body.verificationCode){
+            if (user.length !== 0) {
+                if (user[0].verificationCode === req.body.verificationCode) {
                     res.status(200)
-                    .json({
-                        "response": 'codigo valido',
-                    })
-                } else{
+                        .json({
+                            "response": 'codigo valido',
+                        })
+                } else {
                     res.status(400)
-                    .json({
-                        "response": 'invalid verificacion Code',
-                        "error": "E2009",
-                        "errorDescription": "El código de verificación no coincide con el enviado a tu correo" 
-                    })
+                        .json({
+                            "response": 'invalid verificacion Code',
+                            "error": "E2009",
+                            "errorDescription": "El código de verificación no coincide con el enviado a tu correo"
+                        })
                 }
 
-            } else{
+            } else {
                 res.status(400)
-                .json({
-                    "response": 'User are not registered',
-                    "error": "E2002",
-                    "errorDescription": "Usuario no registrado" 
-                })
+                    .json({
+                        "response": 'User are not registered',
+                        "error": "E2002",
+                        "errorDescription": "Usuario no registrado"
+                    })
             }
 
-        } else{
+        } else {
             res.status(400)
-        .json({
-                "error": "E2010", 
-                "errorDescription" : "Faltan campos en el cuerpo la petición"
-        })
+                .json({
+                    "error": "E2010",
+                    "errorDescription": "Faltan campos en el cuerpo la petición"
+                })
         }
-        
+
     } catch (error) {
         res.status(500)
-        .json({
-                "error": "E2020", 
-                "errorDescription" : "Error inesperado, estamos trabajando en ello"
-        })
+            .json({
+                "error": "E2020",
+                "errorDescription": "Error inesperado, estamos trabajando en ello"
+            })
     }
 }
 
-exports.changePassword = async (req, res, next) => {
+exports.changePassword = async(req, res, next) => {
     try {
-        if(req.body.email && req.body.password){
+        if (req.body.email && req.body.password) {
             let user = await usersService.getOneUser(req.body.email)
-            if(user.length === 0){
+            if (user.length === 0) {
                 res.status(400)
-                .json({
-                    "response": 'User are not registered',
-                    "error": "E2002",
-                    "errorDescription": "Usuario no registrado" 
-                })
-            } else{
+                    .json({
+                        "response": 'User are not registered',
+                        "error": "E2002",
+                        "errorDescription": "Usuario no registrado"
+                    })
+            } else {
                 req.body.password = await bcrypt.encryptPassword(req.body.password)
-                await usersService.updatePassWord(
-                    {"email": req.body.email},
-                    {"password": req.body.password}
-                )
-                await usersService.saveVerificationCode(
-                    {"email": req.body.email},
-                    {"verificationCode": 'Default'}
-                )
+                await usersService.updatePassWord({ "email": req.body.email }, { "password": req.body.password })
+                await usersService.saveVerificationCode({ "email": req.body.email }, { "verificationCode": 'Default' })
                 res.status(200)
-                .json({
-                            "response": "Se cambió la contraseña", 
-                })
-                
+                    .json({
+                        "response": "Se cambió la contraseña",
+                    })
+
             }
-        }else {
+        } else {
             res.status(400).json({
                 "error": "E2024",
                 "errorDescription": "Faltan campos por diligenciar"
             })
         }
-        
+
     } catch (error) {
         res.status(500)
-        .json({
-                "error": "E2020", 
-                "errorDescription" : "Error inesperado, estamos trabajando en ello"
-        })
+            .json({
+                "error": "E2020",
+                "errorDescription": "Error inesperado, estamos trabajando en ello"
+            })
     }
 }
 
--- src/utilities/nodeMailer.js
+++ src/utilities/nodeMailer.js
@@ -1,24 +1,24 @@
 const nodemailer = require('nodemailer');
 const uuid = require('./uuid')
-/* 
-envia un correo electrónico con el código de verificación,
-al usuario que desea cambiar su contraseña
-*/
+    /* 
+    envia un correo electrónico con el código de verificación,
+    al usuario que desea cambiar su contraseña
+    */
 exports.sendVerificationCode = (userEmail, userName) => {
     try {
         let verificationCode = uuid.generateCode();
         let firstName = userName.split(' ')[0]
-        
+
         let transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-            user: 'afrodita.develop@gmail.com',
-            pass: 'afroditadmin'
+                user: 'barbershop2020poli@gmail.com',
+                pass: 'poli.2020'
             }
         });
 
         let mailOptions = {
-            from: 'afrodita.develop@gmail.com',
+            from: 'barbershop2020poli@gmail.com',
             to: userEmail,
             subject: 'Código de verificación para reestablecer tu contraseña',
             html: `
@@ -29,18 +29,18 @@ exports.sendVerificationCode = (userEmail, userName) => {
         };
 
         return transporter.sendMail(mailOptions)
-        .then(resolve => {
-            return {
-                "response": "Se envió el email con exito",
-                "verificationCode": verificationCode
-            }
-        })
-        .catch(reject => {
-            return {
-                "error": "Algo salió mal",
-                "errorDescription": reject
-            }
-        })  
+            .then(resolve => {
+                return {
+                    "response": "Se envió el email con exito",
+                    "verificationCode": verificationCode
+                }
+            })
+            .catch(reject => {
+                return {
+                    "error": "Algo salió mal",
+                    "errorDescription": reject
+                }
+            })
     } catch (error) {
         return {
             "error": "Algo salió mal",
